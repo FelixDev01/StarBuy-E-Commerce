decbd97feb17f43982416f799ccd1161a4d92aa9
FelixVillanuevaCondori <felixeduardobx@gmail.com>
2025-11-27 19:37:30 -0300

    Maven 4.00 mudado para 3.15 pela persistencia com H2

--- src/main/java/com/starbuy/ecommerce/models/User.java
+++ src/main/java/com/starbuy/ecommerce/models/User.java
@@ -1,13 +1,15 @@
 package com.starbuy.ecommerce.models;
 
-import jakarta.persistence.Entity;
-import org.springframework.cglib.core.Local;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "db_user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
